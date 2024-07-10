d5da8e98b2cccb45cd52ad7d8c3bc6fc10c6e06a
Lotzig <lotzig@free.fr>
2024-07-10 11:52:46 +0200

    Frontend : installed Radix React icons and Shadcn alert. Updated NotConnected component for new layout. npm run dev Ok

--- frontend/components/shared/NotConnected.jsx
+++ frontend/components/shared/NotConnected.jsx
@@ -1,8 +1,20 @@
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons"
+ 
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
+
 const NotConnected = () => {
-    return (
-      <div>NotConnected</div>
-    )
-  }
-  
-  export default NotConnected
-  
+  return (
+<Alert variant="destructive">
+      <ExclamationTriangleIcon className="h-4 w-4" />
+      <AlertTitle>Warning</AlertTitle>
+      <AlertDescription>
+        Please connect your Wallet to our DApp.
+      </AlertDescription>
+    </Alert>  )
+}
+
+export default NotConnected
