6d12b85bf9910f3242b5e9b53d1e0367a85b8b37
Lotzig <lotzig@free.fr>
2024-07-14 21:23:48 +0200

    NotConnected.jsx : add images for home page

--- frontend/components/shared/NotConnected.jsx
+++ frontend/components/shared/NotConnected.jsx
@@ -6,15 +6,35 @@ import {
   AlertTitle,
 } from "@/components/ui/alert"
 
+import Image from 'next/image'
+import Accueil1 from '@/images/Accueil1.png'
+import Accueil2 from '@/images/Accueil2.png'
+
+
 const NotConnected = () => {
   return (
-<Alert variant="destructive">
-      <ExclamationTriangleIcon className="h-4 w-4" />
-      <AlertTitle>Warning</AlertTitle>
-      <AlertDescription>
-        Please connect your Wallet to our DApp.
-      </AlertDescription>
-    </Alert>  )
+    <>
+      <div className="flex flex-col space-y-4 items-center">
+        <Image
+          src={Accueil1}
+          alt="CARBONTREE"
+          //width={500} 
+          //height={500}
+        />
+
+        <Image
+          src={Accueil2}
+          alt="CARBONTREE"
+          //width={1000} automatically provided
+          // height={500} automatically provided
+        />
+      
+        <div className="bg-lime-600 text-2xl text-white p-5">Connect your wallet to purchase CARB-B !</div>
+
+      </div>
+
+    </>
+  )
 }
 
 export default NotConnected
